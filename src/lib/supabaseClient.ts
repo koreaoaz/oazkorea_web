db46466485fd6453559b2bce8c1b333fe2591d43
yumin <yung.ym.lee@gmail.com>
2025-07-07 01:05:02 +0900

    [Fix] supdabaseClient : string type issue

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,11 +1,10 @@
-// src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const key = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-NEXT_PUBLIC_SUPABASE_URL: url;
-NEXT_PUBLIC_SUPABASE_KEY: key;
-
+if (!url || !key) {
+  throw new Error("Supabase 환경 변수가 설정되지 않았습니다.");
+}
 
 export const supabase = createClient(url, key);
