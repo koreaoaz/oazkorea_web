2c3127e5b370c928f5efd1e5fe40f83f27e7f6d4
yumin <yung.ym.lee@gmail.com>
2025-07-11 03:01:45 +0900

    [Update] supabaseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 if (!url || !key) {
   throw new Error("Supabase 환경 변수가 설정되지 않았습니다.");
