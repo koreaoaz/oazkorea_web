b2b7d0005a475c62e3ef286a277b6af3a8561228
yumin <yung.ym.lee@gmail.com>
2026-02-20 14:05:08 +0900

    [Update] constant

--- src/app/editor/constants.ts
+++ src/app/editor/constants.ts
@@ -1,6 +1,6 @@
 
 
-export type BoardType = "공지" | "프로젝트" | "스터디" | "학회실 사용 시간표" | "명예의 전당" | "일정" | "행사"
+export type BoardType = "공지" | "프로젝트" | "스터디" | "학회실 사용 시간표" | "명예의 전당" | "일정" | "행사" | "등록회원" | "승인email" | "관리자id"
 
 export const BOARD_TABLE_MAP: Record<BoardType, string> = {
   "공지": "editor_0_noti",
@@ -10,7 +10,9 @@ export const BOARD_TABLE_MAP: Record<BoardType, string> = {
   "명예의 전당": "editor_5_donators",
   "일정": "editor_4_schedules",
   "행사": "editor_6_events",
-  "관리자": ""
+  "등록회원": "registered_member",
+  "승인email" : "allowed_user",
+  "관리자id" : "supervisor_id"
 }
 
 export const BOARD_LIST: BoardType[] = [
@@ -20,7 +22,10 @@ export const BOARD_LIST: BoardType[] = [
   "학회실 사용 시간표",
   "명예의 전당",
   "일정",
-  "행사"
+  "행사",
+  "등록회원",
+  "승인email",
+  "관리자id"
 ]
 
 export const CATEGORY_OPTIONS = [
