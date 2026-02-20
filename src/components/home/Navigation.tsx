11ff479c2b49100c962b44ff433332a64903048b
yumin <yung.ym.lee@gmail.com>
2026-02-20 18:04:47 +0900

    [Update] change order

--- src/components/home/Navigation.tsx
+++ src/components/home/Navigation.tsx
@@ -142,8 +142,8 @@ export default function Navigation() {
       name: "Blog",
       href: "/blog/band",
       subItems: [
-        { name: "Members' Tech Blog", href: "/blog/members-tech-blog" },
         { name: "OAZ Band", href: "/blog/band" }
+        { name: "Members' Tech Blog", href: "/blog/members-tech-blog" },
       ]
     },
     {
